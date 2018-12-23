54f0f85d5e86069d6aea4f1a40e508fa631d06ba
Utz Nisslmüller <a01426012@unet.univie.ac.at>
2018-12-23 10:53:36 +0100

    Hall entry form complete

--- src/KinoUI/ManualEntryForms/HallEntry.java
+++ src/KinoUI/ManualEntryForms/HallEntry.java
@@ -1,4 +1,25 @@
 package KinoUI.ManualEntryForms;
 
-public class HallEntry {
+import Extras.Window;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HallEntry extends Window {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton backButton;
+    private JButton submitButton;
+
+    public HallEntry() {
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+            }
+        });
+    }
 }
