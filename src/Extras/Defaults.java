e8d8b86f03afbbafa0b20ae144ee4368f064415f
Utz Nisslmüller <a01426012@unet.univie.ac.at>
2019-01-07 01:08:09 +0100

    LOL F-ING TIMEZONE

--- src/Extras/Defaults.java
+++ src/Extras/Defaults.java
@@ -2,7 +2,7 @@ package Extras;
 
 public class Defaults {
 
-    public static String databaseURL = "jdbc:mysql://localhost/cinebase" /* + "?verifyServerCertificate=false" + "&useSSL=true" + "&requireSSL=true"*/;
+    public static String databaseURL = "jdbc:mysql://localhost/cinebase?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC" /* + "?verifyServerCertificate=false" + "&useSSL=true" + "&requireSSL=true"*/;
     public static String scriptFolder = "scripts\\";
     //for login data, see SQLHandling -> LoginDataProvider.java
 
