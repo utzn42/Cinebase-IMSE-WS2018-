89648c0fb6b8bd5f9c145053610516caa9edd1b0
oliver <oliver.schweiger@gmail.com>
2019-01-27 01:30:24 +0100

    continued work on screenings php

--- MS3/mongo/screening.php
+++ MS3/mongo/screening.php
@@ -99,202 +99,7 @@ error_reporting(E_ALL ^ E_NOTICE);
 			}
         ?>
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		    <table style='border: 1px solid #DDDDDD'>
-            <thead>
-            <tr id="tableRow">
-                <th id="hallName" style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbyhall=true">Screening ID</a></th>
-                <th id="filmTitle" style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbytitle=true">Film ID</a></th>
-                <th style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbystarting=true">Hall ID</a></th>
-                <th style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbydur=true">Starting Time</a></th>
-
-            </tr>
-            </thead>
-            <tbody>
-		
-		<?php
-			
-
-		
-			try {
-
-				$mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-
-	
-
-				if (isset($_GET['searchFilmID'])) {
-
-					$filter = [ 'title' => new MongoDB\BSON\Regex($_GET['searchTitle'], 'i') ]; 
-					$query = new MongoDB\Driver\Query($filter); 
-
-				} else if (isset($_GET['today'])) {
-					$filter = [ '_id' => $_GET['searchFilmID'] ]; 
-					$query = new MongoDB\Driver\Query($filter); 
-
-				} else if (isset($_GET['tomorrow'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ 'title' => 1]]); 
-
-				} else if (isset($_GET['nextweek'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ 'director' => 1]]); 
-					
-				} else if (isset($_GET['searchTitle'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ 'country' => 1]]); 
-				} 
-				
-				else if (isset($_GET['sortbyhall'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ 'film_language' => 1]]); 
-				} else if (isset($_GET['sortbystarting'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ 'duration' => 1]]); 
-				} else if (isset($_GET['sortbyfilm'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ 'age_rating' => 1]]); 
-				} else if (isset($_GET['sortbytitle'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ '_id' => 1]]); 
-				} else if (isset($_GET['sortbydur'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ '_id' => 1]]); 
-				} else if (isset($_GET['sortbyscreening'])) {
-					$query = new MongoDB\Driver\Query([], ['sort' => [ '_id' => 1]]); 
-				} else {
-					$query = new MongoDB\Driver\Query([]);
-				}
-					
-				
-				 
-				$rows = $mng->executeQuery("cinebase.films", $query);
-				$idx=0;
-				foreach ($rows as $row) {
-					
-					$idx++;
-
-                    if (isset($_SESSION['loggedinEmployee']) && $_SESSION['loggedinEmployee'] == true) {
-                      //  echo "<td style=\"padding: 5px 10px 5px 10px;\">$row->screenings->hall_id</td>";
-                    }
-					$count=0;
-					
-
-					foreach($row->screenings as $key => $value) {
-						echo "<tr>";
-
-						$temp_id = $row->screenings[$count]->_id;
-						echo "<td style=\"padding: 5px 10px 5px 10px;\">$temp_id</td>";
-						
-						$temp_film_id = $row->screenings[$count]->film_id;
-						echo "<td style=\"padding: 5px 10px 5px 10px;\">$temp_film_id</td>";
-
-						$temp_hall_id = $row->screenings[$count]->hall_id;
-						echo "<td style=\"padding: 5px 10px 5px 10px;\">$temp_hall_id</td>";
-						
-						$temp_starting_time = $row->screenings[$count]->starting_time;
-						echo "<td style=\"padding: 5px 10px 5px 10px;\">$temp_starting_time</td>";
-						
-						
-						echo "<td><a href=\"movies.php?searchFilmID=$temp_id\"> Show Film Details </a></td>";
-
-						echo "</tr>";
-
-						$count++;
-					}
-
-					
-					
-			
-
-					//echo '<pre>'; print_r($row->screenings); echo '</pre>';
-
-					
-                    if (isset($_SESSION['loggedinEmployee']) && $_SESSION['loggedinEmployee'] == true) {
-                        
-						//UPDATE BUTTON
-						//echo "<td><a href=\"updatefilm.php?film_id=$row->_id&title=$row->title&director=$row->director&country=$row->country&film_language=$row->film_language&age_rating=$row->age_rating&duration=$row->duration\"> UPDATE </a></td>";
-                        echo "<td>";
-						echo "<form method='post' action='updatescreening.php' class='inline'>";
-						echo "<input type='hidden' name='screening_id' value=$row(screening)->_id>";
-										
-						$str_title = urlencode($row->title);  
-						echo "<input type='hidden' name='film_id' value=$row(screening)->film_id>";
-
-						echo "<input type='hidden' name='hall_id' value=$row(screening)->hall_id>";
-						
-						
-						$str_starting = urlencode($row(screening)->starting_time);  
-						echo "<input type='hidden' name='starting_time' value=$str_starting>";
-						
-						echo "<button type='submit' name='submit_param' value='submit_value' class='link-button'>";
-						echo "UPDATE";
-						echo "</button>";
-						echo "</form>";
-					    echo "</td>";
-
-
-						
-						//DELETE BUTTON
-						//echo "<td><a href=\"movies.php?del=$row->_id\"> DELETE </a></td>";
-						echo "<td>";
-						echo "<form action='screening.php' method='post'>";
-						echo "<input type='hidden' name='del' value=$row(screening)->_id>";
-						echo "<button>DELETE</button>" ;
-						echo "</form>";
-					    echo "</td>";
-
-				
-                    }
-             
-			 
-
-				}
-				
-			} catch (MongoDB\Driver\Exception\Exception $e) {
-
-				$filename = basename(__FILE__);
-				
-				echo "The $filename script has experienced an error.\n"; 
-				echo "It failed with the following exception:\n";
-				
-				echo "Exception:", $e->getMessage(), "\n";
-				echo "In file:", $e->getFile(), "\n";
-				echo "On line:", $e->getLine(), "\n";       
-			}
-
-            
-
-
-            ?>
-		
-		    </tbody>
-			</table>
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
-
-
-        <br>
-
-        <div id="insertScreening">
+		<div id="insertScreening">
             <form id='insertform' action='screening.php' method='post'>
                 Add new screening:
 
@@ -363,69 +168,200 @@ error_reporting(E_ALL ^ E_NOTICE);
         echo("<script type=\"text/javascript\">hideFormInsertScreening();</script>");
 
         ?>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		    <table style='border: 1px solid #DDDDDD'>
+            <thead>
+            <tr id="tableRow">
+                <th id="hallName" style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbyhall=true">Screening ID</a></th>
+                <th id="filmTitle" style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbytitle=true">Film ID</a></th>
+				<th style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbystarting=true">Title</a></th>
+                <th style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbystarting=true">Hall ID</a></th>
+			
+                <th style="padding: 0px 10px 0px 10px;"><a href="screening.php?sortbydur=true">Starting Time</a></th>
+
+            </tr>
+            </thead>
+            <tbody>
+		
+		<?php
+			
+
+		
+			try {
+
+				$mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+
+	
+
+				if (isset($_GET['searchFilmID'])) {
+
+					$filter = [ 'title' => new MongoDB\BSON\Regex($_GET['searchTitle'], 'i') ]; 
+					$query = new MongoDB\Driver\Query($filter); 
+
+				} else if (isset($_GET['today'])) {
+					$filter = [ '_id' => $_GET['searchFilmID'] ]; 
+					$query = new MongoDB\Driver\Query($filter); 
+
+				} else if (isset($_GET['tomorrow'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ 'title' => 1]]); 
+
+				} else if (isset($_GET['nextweek'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ 'director' => 1]]); 
+					
+				} else if (isset($_GET['searchTitle'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ 'country' => 1]]); 
+				} 
+				
+				else if (isset($_GET['sortbyhall'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ 'film_language' => 1]]); 
+				} else if (isset($_GET['sortbystarting'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ 'duration' => 1]]); 
+				} else if (isset($_GET['sortbyfilm'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ 'age_rating' => 1]]); 
+				} else if (isset($_GET['sortbytitle'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ '_id' => 1]]); 
+				} else if (isset($_GET['sortbydur'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ '_id' => 1]]); 
+				} else if (isset($_GET['sortbyscreening'])) {
+					$query = new MongoDB\Driver\Query([], ['sort' => [ '_id' => 1]]); 
+				} else {
+					$query = new MongoDB\Driver\Query([]);
+				}
+					
+				 
+				$rows = $mng->executeQuery("cinebase.films", $query);
+				$idx=0;
+				foreach ($rows as $row) {
+					
+					$idx++;
+
+                    if (isset($_SESSION['loggedinEmployee']) && $_SESSION['loggedinEmployee'] == true) {
+                      //  echo "<td style=\"padding: 5px 10px 5px 10px;\">$row->screenings->hall_id</td>";
+                    }
+					$count=0;
+					
+					if (is_array($row->screenings) || is_object($row->screenings)){
+						foreach($row->screenings as $key => $value) {
+
+							
+							echo "<tr>";
+
+							$temp_id = $row->screenings[$count]->_id;
+							echo "<td style=\"padding: 5px 10px 5px 10px;\">$temp_id</td>";
+							
+							$title=$row->title;
+							$temp_film_id = $row->_id;
+							echo "<td style=\"padding: 5px 10px 5px 10px;\">$temp_film_id</td>";
+							
+							echo "<td style=\"padding: 5px 10px 5px 10px;\">$title</td>";
+
+							$temp_hall_id = $row->screenings[$count]->hall_id;
+							echo "<td style=\"padding: 5px 10px 5px 10px;\">$temp_hall_id</td>";
+							
+							$temp_starting_time = $row->screenings[$count]->starting_time;
+							
+							$datetime = $temp_starting_time->toDateTime();
+
+							$time = strtotime($utcdatetime.' UTC');
+							$formatted = date("Y-m-d H:i", $time);
+							echo "<td style=\"padding: 5px 10px 5px 10px;\">$formatted</td>";
+							
+							
+
+							if (isset($_SESSION['loggedinEmployee']) && $_SESSION['loggedinEmployee'] == true) {
+							
+								//UPDATE BUTTON
+								echo "<td>";
+								echo "<form method='post' action='updatescreening.php' class='inline'>";
+								echo "<input type='hidden' name='screening_id' value=$temp_id>";
+												
+								$str_title = urlencode($row->title);  
+								echo "<input type='hidden' name='film_id' value=$temp_film_id>";
+
+								echo "<input type='hidden' name='hall_id' value=$temp_hall_id>";
+								
+								
+								$str_starting = urlencode($temp_starting_time);  
+								echo "<input type='hidden' name='starting_time' value=$str_starting>";
+								
+								echo "<button type='submit' name='submit_param' value='submit_value' class='link-button'>";
+								echo "UPDATE";
+								echo "</button>";
+								echo "</form>";
+								echo "</td>";
+
+
+								
+								//DELETE BUTTON
+								echo "<td>";
+								echo "<form action='screening.php' method='post'>";
+								echo "<input type='hidden' name='del' value=temp_id>";
+								echo "<button>DELETE</button>" ;
+								echo "</form>";
+								echo "</td>";
+
+					
+							}
+							echo "<td><a href=\"movies.php?searchFilmID=$temp_film_id\"> Show Film Details </a></td>";
+
+						
+							
+							echo "</tr>";
+
+							$count++;
+						}
+					
+
+					
+					
+			
+
+					//echo '<pre>'; print_r($row->screenings); echo '</pre>';
+
+					
+                    
+             
+				}	
+
+				}
+				
+			} catch (MongoDB\Driver\Exception\Exception $e) {
+
+				$filename = basename(__FILE__);
+				
+				echo "The $filename script has experienced an error.\n"; 
+				echo "It failed with the following exception:\n";
+				
+				echo "Exception:", $e->getMessage(), "\n";
+				echo "In file:", $e->getFile(), "\n";
+				echo "On line:", $e->getLine(), "\n";       
+			}
+
+            
+
+
+            ?>
+		
+		    </tbody>
+			</table>
+		
+		
+
+
+        
         <br>
 
 
 
-            <?php
-
-            // fetch rows of the executed sql query
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-
-                    $date = $row['starting_time'];
-                    $simpleDate = new DateTime($date);
-
-                    echo "<tr>";
-                    if (isset($_SESSION['loggedinEmployee']) && $_SESSION['loggedinEmployee'] == true) {
-                        echo "<td>" . $row['screening_id'] . "</td>";
-                        echo "<td>" . $row['hall_id'] . "</td>";
-                    }
-
-
-                    echo "<td>" . $row['name'] . "</td>";
-
-                    if (isset($_SESSION['loggedinEmployee']) && $_SESSION['loggedinEmployee'] == true) {
-                        echo "<td>" . $row['film_id'] . "</td>";
-                    }
-
-                    echo "<td style=\"padding: 0px 10px 0px 10px;\">" . $row['title'] . "</td>";
-                    echo "<td style=\"padding: 0px 10px 0px 10px;\">" . $simpleDate->format('d/m/Y H:i') . "</td>";
-                    echo "<td style=\"padding: 0px 10px 0px 10px;\">" . $row['duration'] . "</td>";
-
-                    if (isset($_SESSION['loggedinEmployee']) && $_SESSION['loggedinEmployee'] == true) {
-                        echo "<td><a href=\"updatescreening.php?screening_id=" . $row['screening_id'] . "&hall_id=" . $row['hall_id'] . "&film_id=" . $row['film_id'] . "&starting_time=" . $row['starting_time'] . "\"> UPDATE </a></td>";
-                        echo "<td><a href=\"deletescreening.php?id=" . $row['screening_id'] . "\"> DELETE </a></td>";
-                    }
-                    echo "<td style=\"padding: 5px 10px 5px 10px;\"><a href=\"movies.php?searchFilmID=" . $row['film_id'] . "\"> Show Film Details </a></td>";
-                    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-                        echo "<td style=\"padding: 5px 10px 5px 10px;\"><a href=\"reserveticket.php?screening_id=" . $row['screening_id'] . "\"> RESERVE </a></td>";
-                    }
-                    echo "</tr>";
-                }
-            }
-            $row_cnt = mysqli_num_rows($result);
-
-
-            ?>
-            </tbody>
-        </table>
-
-
-        <?php
-        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-            $username = $_SESSION['username'];
-            echo("<script type=\"text/javascript\">setLoggedIn(\"$username\");</script>");
-        }
-        if (isset($_SESSION['loggedinAdmin']) && $_SESSION['loggedinAdmin'] == true) {
-            echo("<script type=\"text/javascript\">setAdminMode();</script>");
-        }
-        if (isset($_SESSION['loggedinEmployee']) && $_SESSION['loggedinEmployee'] == true) {
-            $username = $_SESSION['username'];
-            echo("<script type=\"text/javascript\">setEmployeeMode(\"$username\");</script>");
-            echo("<script type=\"text/javascript\">displayScreeningIDs();</script>");
-        }
-        ?>
+ 
 
     </div>
 </div>
