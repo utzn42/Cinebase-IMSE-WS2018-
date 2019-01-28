260ff6e9c393aa6882f53ec3f8267638cda83269
Alexander Ramharter <a01363460@unet.univie.ac.at>
2019-01-28 02:06:39 +0100

    customer can delete his account

--- MS3/mongo/deleteAccount.php
+++ MS3/mongo/deleteAccount.php
@@ -2,22 +2,25 @@
 
 $id = $_GET['id'];
 
-$user = 'root';
-$pass = '';
-$database = 'cinebase';
+try {
 
-echo $id;
+    $mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+    $bulk = new MongoDB\Driver\BulkWrite;
 
-$conn = new mysqli('localhost', $user, $pass, $database) or die("dead");
+    $bulk->delete(['_id' => intval($id)]);
 
-// sql to delete a record
-$sql = "DELETE FROM customer WHERE customer_id = $id";
+    $mng->executeBulkWrite('cinebase.customers', $bulk);
+    header("location: logout.php");
 
-if (mysqli_query($conn, $sql)) {
-    mysqli_close($conn);
-    header('Location: logout.php');
-    exit;
-} else {
-    echo "Error deleting record";
+} catch (MongoDB\Driver\Exception\Exception $e) {
+
+    $filename = basename(__FILE__);
+
+    echo "The $filename script has experienced an error.\n";
+    echo "It failed with the following exception:\n";
+
+    echo "Exception:", $e->getMessage(), "\n";
+    echo "In file:", $e->getFile(), "\n";
+    echo "On line:", $e->getLine(), "\n";
 }
 ?>
